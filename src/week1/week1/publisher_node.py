5d0ddbf842250920d4dd960ad37d779bd414b364
Fida <fidasaifudheen05@gmail.com>
2025-05-31 22:00:01 +0300

    added launch files and qos profile

--- src/week1/week1/publisher_node.py
+++ src/week1/week1/publisher_node.py
@@ -2,10 +2,27 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int32
+from rclpy.qos import (
+    QoSProfile,
+    ReliabilityPolicy,
+    HistoryPolicy,
+    DurabilityPolicy,
+    LivelinessPolicy,
+    Duration
+)
 
 class PublisherNode(Node):
     def __init__(self):
         super().__init__("publisher_node")
+        qos_profile = QoSProfile(
+            reliability=ReliabilityPolicy.RELIABLE,
+            durability=DurabilityPolicy.VOLATILE,
+            history=HistoryPolicy.KEEP_LAST,
+            depth=5,
+            deadline=Duration(nanoseconds=10_000_000),  # 10ms
+            liveliness=LivelinessPolicy.MANUAL_BY_TOPIC,
+            liveliness_lease_duration=Duration(seconds=1)
+        )
         self.num_pub=self.create_publisher(Int32,"/number",10)
         self.num=1
         self.timer=self.create_timer(1.0,self.publish_num)
