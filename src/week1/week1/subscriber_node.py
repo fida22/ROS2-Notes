f32806fcc867f57d50606772b973c52b93bb8bc0
FIDA SAIFUDHEEN <100432271+fida22@users.noreply.github.com>
2025-05-31 22:04:39 +0300

    Update subscriber_node.py

--- src/week1/week1/subscriber_node.py
+++ src/week1/week1/subscriber_node.py
@@ -19,7 +19,7 @@ class SubscriberNode(Node):
             durability=DurabilityPolicy.VOLATILE,
             history=HistoryPolicy.KEEP_LAST,
             depth=5,
-            deadline=Duration(nanoseconds=10_000_000),  # 10ms
+            deadline=Duration(nanoseconds=10_000_000),  
             liveliness=LivelinessPolicy.MANUAL_BY_TOPIC,
             liveliness_lease_duration=Duration(seconds=1),
         )
