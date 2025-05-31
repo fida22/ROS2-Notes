d8cc88c628b74a007a42162bf0f79e23e8629fc2
Fida <fidasaifudheen05@gmail.com>
2025-05-31 22:03:22 +0300

    qos profile added

--- src/week1/week1/subscriber_node.py
+++ src/week1/week1/subscriber_node.py
@@ -2,10 +2,28 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int32
+from rclpy.qos import (
+    QoSProfile,
+    ReliabilityPolicy,
+    HistoryPolicy,
+    DurabilityPolicy,
+    LivelinessPolicy,
+    Duration
+)
 
 class SubscriberNode(Node):
     def __init__(self):
         super().__init__("subscriber_node")
+        qos_profile = QoSProfile(
+            reliability=ReliabilityPolicy.RELIABLE,
+            durability=DurabilityPolicy.VOLATILE,
+            history=HistoryPolicy.KEEP_LAST,
+            depth=5,
+            deadline=Duration(nanoseconds=10_000_000),  # 10ms
+            liveliness=LivelinessPolicy.MANUAL_BY_TOPIC,
+            liveliness_lease_duration=Duration(seconds=1),
+        )
+        
         self.num_sub=self.create_subscription(Int32,"/number",self.square,10)
     
     def square(self,msg):
